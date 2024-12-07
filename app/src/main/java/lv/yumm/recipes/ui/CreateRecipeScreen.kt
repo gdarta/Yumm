69a8803ce7126fb889de53c5cfb85ebfc648747f
dartagobleja <darta.gobleja@mitigate.dev>
2024-12-07 19:47:20 +0200

    prettier ingredient card

--- app/src/main/java/lv/yumm/recipes/ui/CreateRecipeScreen.kt
+++ app/src/main/java/lv/yumm/recipes/ui/CreateRecipeScreen.kt
@@ -15,7 +15,9 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -25,6 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import lv.yumm.recipes.RecipeEvent
@@ -32,6 +37,7 @@ import lv.yumm.recipes.RecipeUiState
 import lv.yumm.recipes.data.Ingredient
 import lv.yumm.ui.theme.BottomNavBar
 import lv.yumm.ui.theme.TopBar
+import lv.yumm.ui.theme.Typography
 import lv.yumm.ui.theme.YummTheme
 import lv.yumm.ui.theme.recipeTextFieldColors
 
@@ -44,7 +50,9 @@ fun CreateRecipeScreen(
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     LazyColumn(
-        modifier = Modifier.padding(all = 10.dp),
+        modifier = Modifier
+            .padding(all = 10.dp)
+            .padding(horizontal = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
@@ -54,10 +62,11 @@ fun CreateRecipeScreen(
                 onValueChange = {
                     title = it
                 },
+                textStyle = Typography.titleLarge,
                 label = { Text(text = "Recipe Title") },
                 singleLine = true,
                 colors = recipeTextFieldColors(),
-                modifier = Modifier.fillMaxWidth(0.9f)
+                modifier = Modifier.fillMaxWidth()
             )
         }
         item {
@@ -69,12 +78,11 @@ fun CreateRecipeScreen(
                 label = { Text(text = "Recipe Description") },
                 maxLines = 5,
                 colors = recipeTextFieldColors(),
-                modifier = Modifier.fillMaxWidth(0.9f)
+                modifier = Modifier.fillMaxWidth()
             )
         }
         itemsIndexed(uiState.ingredients) { index, ingredient ->
             IngredientCard(uiState,
-                ingredient,
                 onNameChange = {
                     onEvent(
                         RecipeEvent.UpdateIngredient(
@@ -87,7 +95,7 @@ fun CreateRecipeScreen(
                     onEvent(
                         RecipeEvent.UpdateIngredient(
                             index,
-                            ingredient.copy(amount = if (it.isNotEmpty()) (it.toFloatOrNull()) else null)
+                            ingredient.copy(amount = it.toFloatOrNull())
                         )
                     )
                 },
@@ -104,7 +112,7 @@ fun CreateRecipeScreen(
             Button(
                 content = { Text(text = "Add an ingredient") },
                 shape = RoundedCornerShape(3.dp),
-                modifier = Modifier.fillMaxWidth(0.9f),
+                modifier = Modifier.fillMaxWidth(),
                 onClick = { onEvent(RecipeEvent.AddIngredient()) }
             )
         }
@@ -112,7 +120,7 @@ fun CreateRecipeScreen(
             Button(
                 content = { Text(text = "Save recipe") },
                 shape = RoundedCornerShape(3.dp),
-                modifier = Modifier.fillMaxWidth(0.9f),
+                modifier = Modifier.fillMaxWidth(),
                 onClick = {
                     onEvent(RecipeEvent.CreateRecipe(title, description))
                     navigateToRecipesScreen()
@@ -126,38 +134,74 @@ fun CreateRecipeScreen(
 @Composable
 fun IngredientCard(
     uiState: RecipeUiState,
-    ingredient: Ingredient,
     onNameChange: (String) -> Unit,
     onAmountChange: (String) -> Unit,
     onMeasurementChange: (String) -> Unit
 ) {
-    Column(
-        modifier = Modifier.padding(vertical = 10.dp)
-    ) {
-        TextField(
-            value = ingredient.name,
-            onValueChange = {
-                onNameChange(it)
-            },
-            label = { Text(text = "Ingredient") },
-            modifier = Modifier,
-            colors = recipeTextFieldColors()
+    var ingredient by remember { mutableStateOf("") }
+    var ingredientField by remember { mutableStateOf(
+        TextFieldValue(
+            text = ingredient,
+            selection = TextRange(ingredient.length)
         )
-        Row {
-            ExposedDropdownMenuFilter(
-                input = ingredient.amount?.toString() ?: "",
-                label = "Amount",
-                filterOptions = { uiState.filteredAmountValues(it) },
-                onValueChange = { onAmountChange(it) },
-                modifier = Modifier.weight(1f)
+    ) }
+    var amount by remember { mutableStateOf("") }
+    var amountField by remember { mutableStateOf(
+        TextFieldValue(
+            text = amount,
+            selection = TextRange(amount.length)
+        )
+    ) }
+    var unit by remember { mutableStateOf("") }
+    var unitField by remember { mutableStateOf(
+        TextFieldValue(
+            text = unit,
+            selection = TextRange(unit.length)
+        )
+    ) }
+    Surface(
+        shape = RoundedCornerShape(5.dp),
+        shadowElevation = 3.dp,
+        tonalElevation = 3.dp,
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = Modifier.padding(all = 10.dp)
+        ) {
+            TextField(
+                value = ingredientField,
+                onValueChange = {
+                    ingredientField = it
+                    onNameChange(it.text)
+                },
+                label = { Text(text = "Ingredient") },
+                modifier = Modifier.fillMaxWidth(),
+                colors = recipeTextFieldColors()
             )
-            ExposedDropdownMenuFilter(
-                input = ingredient.unit,
-                label = "Measurement",
-                filterOptions = { uiState.filteredMsrValues(it) },
-                onValueChange = { onMeasurementChange(it) },
-                modifier = Modifier.weight(1f)
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                ExposedDropdownMenuFilter(
+                    input = amountField,
+                    label = "Amount",
+                    filterOptions = { uiState.filteredAmountValues(it) },
+                    onValueChange = {
+                        amountField = it
+                        onAmountChange(it.text)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+                ExposedDropdownMenuFilter(
+                    input = unitField,
+                    label = "Unit",
+                    filterOptions = { uiState.filteredMsrValues(it) },
+                    onValueChange = {
+                        unitField = it
+                        onMeasurementChange(it.text)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
     }
 }
@@ -165,10 +209,10 @@ fun IngredientCard(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExposedDropdownMenuFilter(
-    input: String = "",
+    input: TextFieldValue,
     label: String = "",
     filterOptions: (String) -> List<String>,
-    onValueChange: (String) -> Unit,
+    onValueChange: (TextFieldValue) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableStateOf(false) }
@@ -181,25 +225,31 @@ fun ExposedDropdownMenuFilter(
             value = input,
             onValueChange = {
                 onValueChange(it)
-                if (filterOptions(input).isNotEmpty()) {
+                if (filterOptions(input.text).isNotEmpty()) {
                     expanded = true
                 }
             },
+            maxLines = 1,
             label = { Text(text = label) },
             modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable),
             colors = recipeTextFieldColors()
         )
-        if (filterOptions(input).isNotEmpty()) {
+        if (filterOptions(input.text).isNotEmpty()) {
             ExposedDropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
             ) {
-                filterOptions(input).forEach { option ->
+                filterOptions(input.text).forEach { option ->
                     DropdownMenuItem(
                         text = { Text(text = option.toString()) },
                         onClick = {
                             expanded = false
-                            onValueChange(option)
+                            onValueChange(
+                                TextFieldValue(
+                                    text = option,
+                                    selection = TextRange(option.length)
+                                )
+                            )
                         }
                     )
                 }
